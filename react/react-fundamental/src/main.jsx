42459c7057e68749385eaf26db35b37fb484a5f1
Hafidzurrohman Saifullah <hafidzurrohmansaifullah@gmail.com>
2025-08-17 11:56:02 +0700

    react learning progress

---
+++ react/react-fundamental/src/main.jsx
@@ -0,0 +1,11 @@
+// entry point atau pertama kali dipanggil
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode> //for dev app react
+    <App /> // komponen yang di import, dan di render dengan react dom
+  </StrictMode>,
+)
